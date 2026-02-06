b23e986bb7f15a720eb53a00ec6c84b69d8bc3ed
gfedorov7 <fedorov.programming@gmail.com>
2026-02-06 13:05:27 +0300

    add const for default values and update exe files

--- internal/parser/flag.go
+++ internal/parser/flag.go
@@ -4,14 +4,19 @@ import (
 	"flag"
 )
 
+const (
+	network = "tcp"
+	address = "127.0.0.1:8080"
+)
+
 type ConsoleFlag struct {
 	Network string
 	Address string
 }
 
 func ParseFlag(consoleFlag *ConsoleFlag) {
-	flag.StringVar(&consoleFlag.Network, "network", "tcp", "network protocol")
-	flag.StringVar(&consoleFlag.Address, "address", ":8080", "address")
+	flag.StringVar(&consoleFlag.Network, "network", network, "network protocol")
+	flag.StringVar(&consoleFlag.Address, "address", address, "address")
 
 	flag.Parse()
 }
